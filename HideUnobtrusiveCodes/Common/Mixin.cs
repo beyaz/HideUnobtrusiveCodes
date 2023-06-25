85ecd938d7c91b3677ec9c466f765bfe00f90b34
beyaz <beyaz1404@gmail.com>
2023-06-25 23:11:24 +0300

    .

--- HideUnobtrusiveCodes/Common/Mixin.cs
+++ HideUnobtrusiveCodes/Common/Mixin.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using HideUnobtrusiveCodes.Dataflow;
 using HideUnobtrusiveCodes.Processors;
@@ -11,6 +12,42 @@ namespace HideUnobtrusiveCodes.Common
     /// </summary>
     static class Mixin
     {
+        /// <summary>
+        /// Returns the index of the first element in the sequence 
+        /// that satisfies a condition.
+        /// </summary>
+        /// <typeparam name="TSource">
+        /// The type of the elements of <paramref name="source"/>.
+        /// </typeparam>
+        /// <param name="source">
+        /// An <see cref="IEnumerable{T}"/> that contains
+        /// the elements to apply the predicate to.
+        /// </param>
+        /// <param name="predicate">
+        /// A function to test each element for a condition.
+        /// </param>
+        /// <returns>
+        /// The zero-based index position of the first element of <paramref name="source"/>
+        /// for which <paramref name="predicate"/> returns <see langword="true"/>;
+        /// or -1 if <paramref name="source"/> is empty
+        /// or no element satisfies the condition.
+        /// </returns>
+        public static int IndexOf<TSource>(this IEnumerable<TSource> source, 
+            Func<TSource, bool> predicate)
+        {
+            int i = 0;
+
+            foreach (TSource element in source)
+            {
+                if (predicate(element))
+                    return i;
+
+                i++;
+            }
+
+            return -1;
+        }
+        
         #region Public Methods
         /// <summary>
         ///     Gets the first character index has value.
